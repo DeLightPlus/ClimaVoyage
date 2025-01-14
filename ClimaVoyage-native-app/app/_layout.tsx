8b6d0a7dd3c75701a3d5b39be735e0dfb252c9d0
DeLightPlus <matlakalakabelo1@gmail.com>
2025-01-14 10:07:50 +0200

    update | initial project setup
    update | initial project setup

--- ClimaVoyage-native-app/app/_layout.tsx
+++ ClimaVoyage-native-app/app/_layout.tsx
@@ -1,5 +1,32 @@
 import { Stack } from "expo-router";
+import { Provider } from 'react-redux';
 
-export default function RootLayout() {
-  return <Stack />;
+import store from '@/redux/store';
+import { SessionProvider } from "@/context/AuthContext";
+
+export default function RootLayout() 
+{
+  return (
+    <Provider store={store}>
+      <SessionProvider>
+        <Stack 
+          screenOptions={{
+            headerStyle: { backgroundColor: "#6200ea" },
+            headerTintColor: "#fff",
+            headerTitleStyle: { fontWeight: "bold" },
+            headerShown: false,
+          }}
+        >
+          <Stack.Screen name="(onboarding)/splash" options={{ headerShown: false }} />
+          <Stack.Screen name="(onboarding)/onboarding" options={{ headerShown: false }} />
+          <Stack.Screen name="(auth)/login" />
+          <Stack.Screen name="(auth)/register" />
+          <Stack.Screen name="(app)/home" />
+          <Stack.Screen name="(app)/addShoppingItem" />
+          <Stack.Screen name="(app)/shoppinglist" />
+        </Stack>  
+      </SessionProvider>    
+    </Provider>
+  
+  )
 }
