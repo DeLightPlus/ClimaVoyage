11e35f10d2100f89b0081eb58bf4ad4e33e6199a
DeLightPlus <matlakalakabelo1@gmail.com>
2025-01-14 10:17:32 +0200

    update layout

--- ClimaVoyage-native-app/app/_layout.tsx
+++ ClimaVoyage-native-app/app/_layout.tsx
@@ -30,49 +30,4 @@ export default function RootLayout()
     </Provider>
   
   )
-=======
-import React from 'react';
-import { StyleSheet, View, TextInput, Button } from 'react-native';
-import MapView from 'react-native-maps';
-import { Link } from 'expo-router';
-
-export default function HomeScreen() {
-    const [location, setLocation] = React.useState('');
-
-    return (
-        <View style={styles.container}>
-            <MapView style={styles.map} />
-            <TextInput
-                style={styles.input}
-                placeholder="Enter location"
-                value={location}
-                onChangeText={setLocation}
-            />
-            <Link href={`/details?location=${location}`}>
-                <Button title="Search" />
-            </Link>
-        </View>
-    );
->>>>>>> 0db8ea403947e740648fec16131b1a65f14c1a8e
 }
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-    map: {
-        width: '100%',
-        height: '50%',
-    },
-    input: {
-        height: 40,
-        borderColor: 'gray',
-        borderWidth: 1,
-        marginTop: 10,
-        marginBottom: 10,
-        paddingHorizontal: 10,
-        width: '90%',
-    },
-});
